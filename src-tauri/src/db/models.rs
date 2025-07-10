091efb184f50e43e51fa089399b27ab3db07fd54
Masayuki Yamagishi <inhmty9.97@gmail.com>
2025-07-10 16:24:43 +0900

    一部定数定義を追加

--- src-tauri/src/db/models.rs
+++ src-tauri/src/db/models.rs
@@ -3,9 +3,7 @@ use serde::{Deserialize, Serialize};
 use sqlx::FromRow;
 use uuid::Uuid;
 
-/**
- * ユーザ情報.
- */
+/// ユーザ情報.
 #[derive(Debug, Serialize, Deserialize, FromRow, Clone)]
 pub struct User {
   pub id: Uuid,
@@ -33,10 +31,8 @@ pub struct User {
   pub other_skills: Option<String>,
 }
 
-/**
- * ユーザ情報新規登録用.
- * id, created_at, updated_atはDB側で自動設定されるので不要
- */
+/// ユーザ情報新規登録用.
+/// id, created_at, updated_atはDB側で自動設定されるので不要
 #[derive(Debug, Serialize, Deserialize)]
 pub struct NewUser {
   pub user_identifier: String,
@@ -61,10 +57,8 @@ pub struct NewUser {
   pub other_skills: Option<String>,
 }
 
-/**
- * ユーザ情報更新用.
- * PUT/PATCH想定、全フィールドOptionalで部分更新できるようにする.
- */
+/// ユーザ情報更新用.
+/// PUT/PATCH想定、全フィールドOptionalで部分更新できるようにする.
 #[derive(Debug, Serialize, Deserialize)]
 pub struct UpdateUser {
   pub user_identifier: Option<String>,
@@ -87,4 +81,53 @@ pub struct UpdateUser {
   pub sales_comment: Option<String>,
   pub toeic_score: Option<i32>,
   pub other_skills: Option<String>,
+}
+
+/// 資格マスタ
+#[derive(Debug, Serialize, Deserialize, FromRow, Clone)]
+pub struct Qualification {
+  pub id: String,
+  pub name: String,
+  pub description: Option<String>,
+  pub is_national: bool,
+}
+
+/// スキルカテゴリマスタ
+#[derive(Debug, Serialize, Deserialize, FromRow, Clone)]
+pub struct SkillCategories {
+  pub id: String,
+  pub label: String,
+  pub description: Option<String>,
+}
+
+/// スキルタグマスタ
+#[derive(Debug, Serialize, Deserialize, FromRow, Clone)]
+pub struct SkillTags {
+  pub id: String,
+  pub label: String,
+  pub description: Option<String>,
+}
+
+/// スキルマスタ
+#[derive(Debug, Serialize, Deserialize, FromRow, Clone)]
+pub struct Skill {
+  pub id: String,
+  pub label: String,
+  pub description: Option<String>,
+  pub devicon_id: Option<String>,
+  pub category_id: String,
+}
+
+/// スキルタグ付け
+#[derive(Debug, Serialize, Deserialize, FromRow, Clone)]
+pub struct SkillTagMap {
+  pub skill_id: String,
+  pub tag_id: String,
+}
+
+/// 担当工程マスタ
+#[derive(Debug, Serialize, Deserialize, FromRow, Clone)]
+pub struct MasterProcesses {
+  pub id: i32,
+  pub name: String,
 }
