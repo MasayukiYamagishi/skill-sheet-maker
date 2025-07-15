be05ea3f0ca2c8cab4436b25bec46023360e5901
Masayuki Yamagishi <inhmty9.97@gmail.com>
2025-07-15 18:14:07 +0900

    資格情報API&コマンド実装

--- src-tauri/src/commands/qualifications.rs
+++ src-tauri/src/commands/qualifications.rs
@@ -1,1 +1,13 @@
-// TODO: 資格真歌DBアクセス Tauri用APIコマンド
+use crate::db::{models::Qualification, qualifications};
+use tauri::State;
+
+/// 資格情報全件取得.
+#[tauri::command]
+pub async fn fetch_all_qualifications_command(pool: State<'_, sqlx::PgPool>) -> Result<Vec<Qualification>, String> {
+  qualifications::fetch_all_qualifications(&pool).await.map_err(|e| e.to_string())
+}
+
+/// 指定したIDの資格情報取得.
+pub async fn fetch_qualification_by_id_command(pool: State<'_, sqlx::PgPool>, id: String) -> Result<Option<Qualification>, String> {
+  qualifications::fetch_qualification_by_id(&pool, &id).await.map_err(|e| e.to_string())
+}
